1cb1f4b1a1ca05415eca0c68c80ca9c2816d1d79
Vanesa <vanesa.chiriceanu@yahoo.com>
2022-04-03 15:42:06 +0300

    -changes

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+alert('I exist!');
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
